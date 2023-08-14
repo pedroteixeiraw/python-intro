46722b35b26c35bb884dfa8b137d3528704c482d
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-14 22:50:34 +0100

    Add exercise 2.

--- python-review.py
+++ python-review.py
@@ -1,8 +1,8 @@
 """
-Sleep in:
+[1] Sleep in:
 The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
-example:
+Example:
 sleep_in(False, False) → True
 sleep_in(True, False) → False
 sleep_in(False, True) → True
@@ -13,10 +13,24 @@ def spleep_in(weekday, vacation):
         return True
     else:
         return False   
-
-"""
-Another option: 
-
-def spleep_in(weekday,vacation):
+ 
+# or
+def spleep_in2(weekday,vacation):
     return(not weekday or vacation)
+
+
 """
+[2] Diff21
+
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+Example:
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0
+"""
+def diff21(n):
+  if n > 21:
+    return 2 * (n - 21)
+  else:
+    return 21 - n
