7e7f30754ec22093332596587548a7650da84181
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-16 11:12:10 +0100

    More exercices.

--- python-exercises.py
+++ python-exercises.py
@@ -6,7 +6,6 @@ sleep_in(False, False) → True
 sleep_in(True, False) → False
 sleep_in(False, True) → True
 """
-
 def spleep_in(weekday, vacation):
     if not (weekday) or vacation:
         return True
@@ -41,7 +40,6 @@ near_hundred(93) → True
 near_hundred(90) → True
 near_hundred(89) → False
 """
-
 def near_hundred(n):
   if abs(100 - n) <= 10 or abs(200 - n) <= 10:
     return True
@@ -51,3 +49,66 @@ def near_hundred(n):
 # or
 def near_hundred2(n):
   return ((abs(100 - n) <= 10) or (abs(200 - n) <= 10))
+
+"""
+[4] Missing char:
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+"""
+def missing_char(str, n):
+  before  = str[:n]
+  after   = str[n+1:]
+  return before + after
+
+"""
+[5] Monkey trouble
+We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+
+monkey_trouble(True, True) → True
+monkey_trouble(False, False) → True
+monkey_trouble(True, False) → False
+"""
+def monkey_trouble(a_smile, b_smile):
+   return a_smile == b_smile
+
+"""
+[6] Parrot trouble
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+
+parrot_trouble(True, 6) → True
+parrot_trouble(True, 7) → False
+parrot_trouble(False, 6) → False
+"""
+def parrot_trouble(talking, hour):
+  return (talking and (hour < 7 or hour > 20))
+
+"""
+[7] Makes 10
+
+Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → True
+makes10(9, 9) → False
+makes10(1, 9) → True
+"""
+def makes10(a, b):
+   return (a == 10 or b == 10 or a + b == 10)
+
+
+""""
+[8] Sum Double
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+sum_double(1, 2) → 3
+sum_double(3, 2) → 5
+sum_double(2, 2) → 8
+"""
+def sum_double(a, b):
+   sum = a + b
+   if a == b:
+      sum = sum * 2
+   return sum
