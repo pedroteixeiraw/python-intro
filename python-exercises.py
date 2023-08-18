b13b3479654dcd734684f9e9cf3a84a297415135
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-18 23:09:32 +0100

    +Warm Up 2 +Ex 1

--- python-exercises.py
+++ python-exercises.py
@@ -173,3 +173,18 @@ def front3(str):
 
 def front3(str):
    return str[:3] + str[:3] + str[:3]
+
+
+#Warm Up 2
+"""
+[1] Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+"""
+def string_times(str, n):
+  result = ''
+  for i in range(n):
+    result = result + str
+  return(result)
