757e76b50a9196f6462326a9b33a5b59e6fdd1db
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-20 23:34:36 +0100

    +Ex 5

--- python-exercises.py
+++ python-exercises.py
@@ -232,3 +232,24 @@ def string_splosion(str):
   for i in range(len(str)+1):
     result = result + str[:i]
   return result
+
+"""
+[5] Last 2
+
+Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+last2('hixxhi') → 1
+last2('xaxxaxaxx') → 1
+last2('axxxaaxx') → 2
+"""
+def last2(str):
+  if len(str) < 2:
+    return 0
+  
+  last2 = str[-2:]
+  count = 0
+  for i in range(len(str) - 2):
+    if str[i:i+2] == last2:
+      count += 1
+  
+  return count
