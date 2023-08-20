8eb2d23a81833b8cc1a6088ee6237fc07fd9b07f
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-21 00:12:29 +0100

    Update Ex 9

--- python-exercises.py
+++ python-exercises.py
@@ -313,13 +313,12 @@ string_match('abc', 'abc') → 2
 string_match('abc', 'axc') → 0
 """
 def string_match(a, b):
-  shorter = min(len(a), len(b))
+  end = min(len(a), len(b))
   count = 0
-    
-  for i in range(shorter-1):
+  for i in range(end - 1):
     a_sub = a[i:i+2]
     b_sub = b[i:i+2]
     if a_sub == b_sub:
-      count = count + 1
-
+      count += 1
+  
   return count
