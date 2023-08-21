364e19d645d93b8924d7aea00d565e9768f9ee9a
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-21 22:04:47 +0100

    +Ex 4

--- python-exercises.py
+++ python-exercises.py
@@ -356,3 +356,15 @@ make_tags('cite', 'Yay') → '<cite>Yay</cite>'
 """
 def make_tags(tag, word):
   return('<'+tag+'>'+word+'</'+tag+'>')
+
+
+"""
+[4] Make out word
+Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+
+make_out_word('<<>>', 'Yay') → '<<Yay>>'
+make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+make_out_word('[[]]', 'word') → '[[word]]'
+"""
+def make_out_word(out, word):
+  return(out[:2]+word+out[2:])
