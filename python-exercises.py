08d46e404ba71273ddbdcaaa8a6072f6d4bb7283
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-21 21:53:55 +0100

    +Ex 3

--- python-exercises.py
+++ python-exercises.py
@@ -345,3 +345,14 @@ make_abba('What', 'Up') → 'WhatUpUpWhat'
 """
 def make_abba(a, b):
     return(a + b + b + a)
+
+"""
+[3] Make Tags 
+The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+
+make_tags('i', 'Yay') → '<i>Yay</i>'
+make_tags('i', 'Hello') → '<i>Hello</i>'
+make_tags('cite', 'Yay') → '<cite>Yay</cite>'
+"""
+def make_tags(tag, word):
+  return('<'+tag+'>'+word+'</'+tag+'>')
