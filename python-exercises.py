e298882daa61afb0a0b7d9c5fe5368a5c6ec0152
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-21 22:06:57 +0100

    +Ex 5

--- python-exercises.py
+++ python-exercises.py
@@ -368,3 +368,15 @@ make_out_word('[[]]', 'word') → '[[word]]'
 """
 def make_out_word(out, word):
   return(out[:2]+word+out[2:])
+
+"""
+[5] Extra End
+Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+
+extra_end('Hello') → 'lololo'
+extra_end('ab') → 'ababab'
+extra_end('Hi') → 'HiHiHi'
+"""
+def extra_end(str):
+  last_2 = str[-2:]
+  return(last_2+last_2+last_2)
