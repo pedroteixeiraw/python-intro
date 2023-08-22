11cb2a787e67d49abf4654df09cc1c12f4fc9f25
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-22 22:18:11 +0100

    +Ex 1

--- python-exercises.py
+++ python-exercises.py
@@ -457,3 +457,15 @@ left2('Hi') → 'Hi'
 """
 def left2(str):
     return str[2:] + str[:2]
+
+#List 1
+"""
+[1] List 1
+Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+
+first_last6([1, 2, 6]) → True
+first_last6([6, 1, 2, 3]) → True
+first_last6([13, 6, 1, 2, 3]) → False
+"""
+def first_last6(nums):
+  return nums[0] == 6 or nums[-1] == 6
