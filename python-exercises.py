eba6602cf4a196e538d3533d0c86248801951daa
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-22 22:37:54 +0100

    +Ex 5

--- python-exercises.py
+++ python-exercises.py
@@ -500,3 +500,21 @@ common_end([1, 2, 3], [1, 3]) → True
 """
 def common_end(a, b):
   return a[0] == b[0] or a[-1] == b[-1]
+
+"""
+[5] Sum 3
+Given an array of ints length 3, return the sum of all the elements.
+
+sum3([1, 2, 3]) → 6
+sum3([5, 11, 2]) → 18
+sum3([7, 0, 0]) → 7
+"""
+def sum3(nums):
+  return sum(nums)
+
+#or
+def sum3(nums):
+  result = 0
+  for i in range(len(nums)):
+    result += nums[i]
+  return result
