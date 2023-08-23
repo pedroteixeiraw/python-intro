5716f5221c19140dfa856b61ab725a0dde6a4e14
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-23 23:33:12 +0100

    +Ex 11

--- python-exercises.py
+++ python-exercises.py
@@ -583,3 +583,13 @@ middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
 def middle_way(a, b):
   return [a[1]] + [b[1]]
 
+"""
+[11] Make Ends
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+
+make_ends([1, 2, 3]) → [1, 3]
+make_ends([1, 2, 3, 4]) → [1, 4]
+make_ends([7, 4, 6, 2]) → [7, 2]
+"""
+def make_ends(nums):
+  return [nums[0]] + [nums[-1]]
