3eb0f62b9b7c9c8bc1c87e797e52daa731a605f3
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-23 23:26:47 +0100

    +Ex 8

--- python-exercises.py
+++ python-exercises.py
@@ -540,3 +540,18 @@ reverse3([7, 0, 0]) → [0, 0, 7]
 """
 def reverse3(nums):
   return nums[::-1]
+
+"""
+[8] Max End 3
+Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+max_end3([1, 2, 3]) → [3, 3, 3]
+max_end3([11, 5, 9]) → [11, 11, 11]
+max_end3([2, 11, 3]) → [3, 3, 3]
+"""
+def max_end3(nums):
+  if nums[0] > nums[2]:
+    max = nums[0]
+  else:
+    max = nums[2]
+  return [max, max, max]
