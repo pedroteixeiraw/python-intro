4efbe51b05a0728a081b4975e7fef5550150bfab
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-24 23:40:07 +0100

    +Ex 1

--- python-exercises.py
+++ python-exercises.py
@@ -604,3 +604,17 @@ has23([4, 5]) → False
 """
 def has23(nums):
   return nums[0] == 2 or nums[1] == 2 or nums[0] == 3 or nums[1] == 3
+
+# Logic 1
+"""
+[1] Cigar Party
+When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return True if the party with the given values is successful, or False otherwise.
+
+cigar_party(30, False) → False
+cigar_party(50, False) → True
+cigar_party(70, True) → True
+"""
+def cigar_party(cigars, is_weekend):
+  week    = is_weekend == False and (cigars >= 40 and cigars <= 60)
+  weekend = is_weekend == True and cigars >= 40
+  return week or weekend
