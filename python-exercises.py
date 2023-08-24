c7ca89515e055efa2ed3a3df8642c58c73fe714b
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-24 23:57:12 +0100

    +Ex 5

--- python-exercises.py
+++ python-exercises.py
@@ -668,3 +668,17 @@ def caught_speeding(speed, is_birthday):
   else:
     return 2
 
+"""
+[5] Sorta sum
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+sorta_sum(3, 4) → 7
+sorta_sum(9, 4) → 20
+sorta_sum(10, 11) → 21
+"""
+def sorta_sum(a, b):
+  soma = a + b
+  if soma >= 10 and soma <= 19:
+    return 20
+  else:
+    return soma
