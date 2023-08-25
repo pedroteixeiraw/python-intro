2c4e57b71f0fa4646b0aac842498391cfce67958
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-25 22:50:55 +0100

    +Ex 6

--- python-exercises.py
+++ python-exercises.py
@@ -682,3 +682,24 @@ def sorta_sum(a, b):
     return 20
   else:
     return soma
+
+"""
+[6] Alarm clock
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+
+alarm_clock(1, False) → '7:00'
+alarm_clock(5, False) → '7:00'
+alarm_clock(0, False) → '10:00'
+"""
+def alarm_clock(day, vacation):
+  if day in [1,2,3,4,5]:
+    if vacation: 
+      return "10:00" 
+    else: 
+      return "7:00"
+  else:
+    if vacation: 
+      return "off" 
+    else: 
+      return "10:00"
+
