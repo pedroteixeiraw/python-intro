ed840a5fd1019c91f2dabf9b1056987c595bf1d2
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-25 23:16:04 +0100

    +Ex 9

--- python-exercises.py
+++ python-exercises.py
@@ -732,3 +732,12 @@ def in1to10(n, outside_mode):
     return n in [1,2,3,4,5,6,7,8,9,10]
 
 """
+[9] Near ten
+Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+
+near_ten(12) → True
+near_ten(17) → False
+near_ten(19) → True
+"""
+def near_ten(num):
+    return (num % 10 <= 2) or (num % 10 >= 8)
