25d8994c515ab05a4a643495cd0eab4b96d103b0
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-25 23:09:04 +0100

    +Ex 8

--- python-exercises.py
+++ python-exercises.py
@@ -716,3 +716,19 @@ def love6(a, b):
     return True
   else:
     return False
+      
+"""
+[8] In 1 to 10
+Given a number n, return True if n is in the range 1..10, inclusive. Unless outside_mode is True, in which case return True if the number is less or equal to 1, or greater or equal to 10.
+
+in1to10(5, False) → True
+in1to10(11, False) → False
+in1to10(11, True) → True
+"""
+def in1to10(n, outside_mode):
+  if outside_mode:
+    return n <= 1 or n >= 10
+  else:
+    return n in [1,2,3,4,5,6,7,8,9,10]
+
+"""
