1562b134ef1cfa8589df9ad256453837d9394911
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-27 21:47:20 +0100

    +Ex 6

--- python-exercises.py
+++ python-exercises.py
@@ -819,3 +819,20 @@ def fix_teen(num):
 
 
 """
+[5] Round Sum
+For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code repetition, write a separate helper "def round10(num):" and call it 3 times. Write the helper entirely below and at the same indent level as round_sum().
+
+round_sum(16, 17, 18) → 60
+round_sum(12, 13, 14) → 30
+round_sum(6, 4, 4) → 10
+"""
+def round_sum(a, b, c):
+  return round10(a) + round10(b) + round10(c)
+  
+def round10(num):
+  r = num % 10
+  q = num / 10
+  if r >= 5:
+    return (q + 1) * 10
+  else:
+    return q * 10
