9751fc398ae3f973a3044acf518bf1119fad016c
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-27 23:17:24 +0100

    +Ex 2

--- python-exercises.py
+++ python-exercises.py
@@ -882,3 +882,18 @@ def double_char(str):
   for i in str:
     result = result + i + i
   return result
+
+"""
+[2] Count hi
+Return the number of times that the string "hi" appears anywhere in the given string.
+
+count_hi('abc hi ho') → 1
+count_hi('ABChi hi') → 2
+count_hi('hihi') → 2
+"""
+def count_hi(str):
+  count = 0
+  for i in range(len(str)):
+    if str[i:i+2] == 'hi':
+      count += 1
+  return count
