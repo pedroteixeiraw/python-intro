3c0ad28cec4e4204db96d4fdfa32724de6171641
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-27 23:21:12 +0100

    +Ex 3

--- python-exercises.py
+++ python-exercises.py
@@ -897,3 +897,22 @@ def count_hi(str):
     if str[i:i+2] == 'hi':
       count += 1
   return count
+
+
+"""
+[3] Cat Dog
+Return True if the string "cat" and "dog" appear the same number of times in the given string.
+
+cat_dog('catdog') → True
+cat_dog('catcat') → False
+cat_dog('1cat1cadodog') → True
+"""
+def cat_dog(str):
+  count_cat = 0
+  count_dog = 0
+  for i in range(len(str)):
+    if str[i:i+3] == "cat":
+      count_cat += 1
+    elif str[i:i+3] == 'dog':
+      count_dog += 1
+  return count_cat == count_dog
