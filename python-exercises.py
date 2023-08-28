5c1b3ebef7ead90a8a928063c75e0873bf379c9d
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-28 22:27:43 +0100

    +Ex 4

--- python-exercises.py
+++ python-exercises.py
@@ -916,3 +916,19 @@ def cat_dog(str):
     elif str[i:i+3] == 'dog':
       count_dog += 1
   return count_cat == count_dog
+
+
+"""
+[4] Count code
+Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+count_code('aaacodebbb') → 1
+count_code('codexxcode') → 2
+count_code('cozexxcope') → 2
+"""
+def count_code(str):
+  count = 0
+  for i in range(len(str)-3):
+    if str[i] == 'c' and str[i+1] == 'o' and str[i+3] == 'e':
+      count += 1
+  return count
