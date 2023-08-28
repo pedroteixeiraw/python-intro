1b98ea46fcadd9e6f22266c798339ebcc253369f
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-28 22:42:22 +0100

    +Ex 5

--- python-exercises.py
+++ python-exercises.py
@@ -932,3 +932,23 @@ def count_code(str):
     if str[i] == 'c' and str[i+1] == 'o' and str[i+3] == 'e':
       count += 1
   return count
+
+"""
+[5] End other
+Given two strings, return True if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: s.lower() returns the lowercase version of a string.
+
+end_other('Hiabc', 'abc') → True
+end_other('AbC', 'HiaBc') → True
+end_other('abc', 'abXabc') → True
+"""
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+  if len(a) >= len(b):
+    small = b
+    big = a
+    return big[-len(b):] == b
+  else:
+    small = a
+    big = b
+    return big[-len(a):] == a
