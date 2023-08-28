57dc3669c54f2cd945cfa5dfa1ce1a9f9c182d8d
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-28 23:05:52 +0100

    Update Ex 6

--- python-exercises.py
+++ python-exercises.py
@@ -963,8 +963,6 @@ xyz_there('xyz.abc') → True
 """
 def xyz_there(str):
   for i in range(len(str)-2):
-    if str[i:i+3] == 'xyz' and i == 0:
-      return True
-    elif str[i:i+3] == 'xyz' and str[i-1] != '.':
+    if (str[i-1] != '.' or i == 0) and str[i:i+3] == 'xyz':
       return True
   return False
