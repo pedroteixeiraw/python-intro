4edd17c450dc9e87462494b3a3eebc34cce4994f
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-29 23:39:14 +0100

    Update python-exercises.py

--- python-exercises.py
+++ python-exercises.py
@@ -1011,7 +1011,8 @@ def max(v1, v2):
   else:
     return v2
 
-"""
+#Improve!
+""" 
 [3] Centered average
 
 Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
