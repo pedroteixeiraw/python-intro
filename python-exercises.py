ff9d14b0bf7d8a677e67ba731bde17c0061907ac
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-29 23:48:37 +0100

    +Ex 5

--- python-exercises.py
+++ python-exercises.py
@@ -1059,3 +1059,23 @@ def sum13(nums):
     if nums[i] != 13 and ((i > 0 and nums[i-1] != 13) or i == 0):
       sum += nums[i]
   return sum
+
+"""
+[5] Sum 67 
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+"""
+def sum67(nums):
+  flag = 1
+  sum = 0
+  for i in range(len(nums)):
+    if nums[i] == 6:
+      flag = 0
+    elif nums[i] == 7:
+      flag = 1
+    if flag and (nums[i] == 6 or nums[i] == 7):
+      sum += nums[i]
+  return sum
