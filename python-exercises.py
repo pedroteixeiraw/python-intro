854c748766cd78edb66d891e4b1fed20d024dec7
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-29 23:38:52 +0100

    +Ex 4

--- python-exercises.py
+++ python-exercises.py
@@ -1043,3 +1043,18 @@ def max(v1, v2):
     return v1
   else:
     return v2
+
+"""
+[4] Sum 13
+Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+sum13([1, 2, 2, 1]) → 6
+sum13([1, 1]) → 2
+sum13([1, 2, 2, 1, 13]) → 6
+"""
+def sum13(nums):
+  sum = 0
+  for i in range(len(nums)):
+    if nums[i] != 13 and ((i > 0 and nums[i-1] != 13) or i == 0):
+      sum += nums[i]
+  return sum
