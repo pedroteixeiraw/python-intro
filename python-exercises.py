4eefbfc669eae8b18d89278c89759aa5ea3d32d1
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-31 23:53:16 +0100

    Update python-exercises.py

--- python-exercises.py
+++ python-exercises.py
@@ -766,6 +766,15 @@ def make_bricks(small, big, goal):
   else:
       return True
 
+#or
+def make_bricks(small, big, goal):
+  if goal > big*5 + small: 
+    return False
+  if goal % 5 > small:
+    return False
+  return True
+
+
 """
 [2] Lone sum
 We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
