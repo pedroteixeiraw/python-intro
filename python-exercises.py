9a22913cd291c097a5685da5366823ec5d2ee048
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-08-31 23:44:07 +0100

    Update python-exercises.py

--- python-exercises.py
+++ python-exercises.py
@@ -743,7 +743,7 @@ def near_ten(num):
     return (num % 10 <= 2) or (num % 10 >= 8)
 
 #Logic 2
-""" Do again!!!
+"""
 [1] Makes Bricks
 We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops. See also: Introduction to MakeBricks
 
@@ -753,14 +753,17 @@ make_bricks(3, 2, 10) → True
 """
 def make_bricks(small, big, goal):
   n = int(goal / 5)
-  if n == big:
+  if n == big and goal == n*5:
     return True
+  elif n > big:
+    goal2 = goal - big*5
   else:
     goal2 = goal - n*5
-    n2 = goal2 / 1
-    if  n2 > small:
-      return False
-    else:
+  
+  n2 = goal2 / 1
+  if  n2 > small:
+    return False
+  else:
       return True
 
 """
