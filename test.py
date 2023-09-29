4ce82d937139b96366362b55dc40d4821ef5ab80
Pedro Teixeira <pedroteixeiraw@gmail.com>
2023-09-29 23:31:12 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,74 @@
+cv
+
+File outline
+We can’t find any sections or subsections in this file.
+Find out more about the file outline
+Editor mode.
+
+
+Normal text
+Selection deleted
+▾
+▾
+\documentclass[localFont,alternative]{yaac-another-awesome-cv}
+\name{Pedro} {Teixeira}
+\tagline{Physics Engineer at OWL, Portugal}
+\photo{2.5cm}{darwiin}
+\socialinfo{
+        \email{pedroteixeiraw@gmail.com}
+        \smartphone{+351 917 117 887}\\
+        \linkedin{pedroteixeiraw}
+        \twitter{pedroteixeiraw}
+        \github{pedroteixeiraw}\\
+        \address{Braga, Portugal}
+        \infos{Languages: Portuguese, English, German and French.}
+}
+
+\begin{document}
+	\makecvheader
+	\input{section_headline}
+        \input{section_experience_short}
+	\input{section_work_experience}
+	\input{section_competences}
+    \input{section_activities}
+	\input{section_langues}
+\end{document}
+Pedro TEIXEIRA
+Physics Engineer at OWL, Portugal
+� pedroteixeiraw@gmail.com � +351 917 117 887
+� pedroteixeiraw � pedroteixeiraw � pedroteixeiraw
+� Braga, Portugal � Languages : Portuguese, English, German and French.
+I am a Physics Engineer at OWL (Only Watch Leads) in Coimbra, responsible for the development of an AWS API based on satellite
+data processing. I am also a master’s student at the University of Minho in Braga, in the final stage of my master’s program.
+I have a strong interest in data science, machine learning, satellite data, motorsports data analysis, cryptography, solid-state phy-
+sics, performance engineering, economics, and finance. When I’m not working, I spend my time on sports, social groups, learning
+new languages, analyzing F1 races data, and studying the stock market (Europe & US).
+� EDUCATION
+Feb 2024 Master of Science - MS, Engineering Physics/ Information Physics, UNIVERSITY OF MINHO, Portugal
+Feb 2022 ∠ Study Topics : data science, machine learning, quantum programming, quantum logic, data acqui-
+sition systems, microprocessors, language processors, networks and communications, cryptography
+and information security, cyber physics, quantum information and photonics.
+∠ Member of NEFUM (Students Network).
+Jan 2022 Bachelor of Science - BS, Engineering Physics, UNIVERSITY OF MINHO, Portugal
+Sep 2018 ∠ Solid foundation in physics, mathematics, electrical and computer engineering.
+∠ Student Representative and member of NEFUM (Students Network).
+� WORK EXPERIENCE
+Out 2023 Physics Engineer, OWL - ONLY WATCH LEADS, Coimbra, Portugal
+Aug 2023 ∠ Main topics : AWS architecture, Satellite data processing (ESA/Sentinel satellites), object detection
+algorithms (YOLO), and performance engineering.
+∠ Objectives : Develop an API with AWS. Programming language : Python;
+Jul 2023 Teacher, PRIVATE TUTOR, Braga, Portugal
+Jan 2017 ∠ High School subjects : mathematics, physics, chemistry and geometry.
+∠ University subjects : calculus and algebra (for science, economics and finance students); Python and
+C (for science and engineering students);
+� SKILLS
+Programming Languages SQL, Python, Haskell, C and R.
+Software Uppaal, Wolfram Mathematica, MatLab (SimuLink), GitHub, Tanner Tools, Tina-TI (Texas Ins-
+truments), Atmel Studio, Unity, Wireshark and LaTeX
+� ACTIVITIES
+Groups and Volunteering EuroPython Society (Member), Red Cross (Student Fundraiser), University of Minho (Student
+Mentor) and NEFUM (Member).
+Sports Runnig and Swimming (University Team member).
+� LANGUAGES
+Portuguese C2 (Native level)
+English B2 (Intermediate level)
